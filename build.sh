6ae5bd5b64ee04b0c00ffc1b17f875ffcd4ad4b7
lance <g_liang08@126.com>
2020-03-02 22:24:51 +0800

    20200302

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
+# change pip chanaels.
+ln -s ~/dotfiles/pip ~/.pip
+
+# configure for vim.
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+
+# configure for bash.
 ln -s ~/dotfiles/bash/bashrc ~/.bashrc
+ln -s ~/dotfiles/bash/bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/bash/bash_profile ~/.bash_profile
-ln -s ~/dotfiles/pip ~/.pip
